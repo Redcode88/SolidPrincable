c922cae33ca0d37ce3b6e365bbf725fd61fe244a
Medo Mohsen <41289533+Redcode88@users.noreply.github.com>
2022-06-15 11:21:15 +0200

    Add files via upload

---
+++ SolidPrincable/Post.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidPrincable.LSP
+{
+    /// <summary>
+    /// Super Class for base Implemntaion 
+    /// </summary>
+  public class Post
+  {
+   
+    public virtual string CreatePost(PostDatabase db,string post)
+    {
+       return db.Add(post);
+    }
+  }
+
+ public class TagPost : Post
+  {
+        public override string CreatePost(PostDatabase db, string post)
+        {
+            return db.AddTagPost(post);
+        }
+  }
+
+  public class MentionPost : Post
+    {
+        public override string CreatePost(PostDatabase db, string post)
+        {
+            return db.AddMentionPost(post);
+        }
+    }
+
+
+}
