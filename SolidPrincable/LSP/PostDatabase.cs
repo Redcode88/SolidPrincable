50ecb2dd308c1a05c29a270973183c84886d9942
Medo Mohsen <41289533+Redcode88@users.noreply.github.com>
2022-06-15 11:15:44 +0200

    LSP

---
+++ SolidPrincable/LSP/PostDatabase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidPrincable.LSP
+{
+   public class PostDatabase
+    {
+        public string Add(string post)
+        {
+            return "Orignal post :" + post;
+        }
+
+        public string AddTagPost(string post)
+        {
+            return "TagPost post :" + post;
+        }
+
+        public string AddMentionPost(string post)
+        {
+            return "Mention post :" + post;
+        }
+
+
+
+    }
+}
--- SolidPrincable/Program.cs
+++ SolidPrincable/Program.cs
@@ -1,6 +1,9 @@
-﻿using SolidPrincable.OCP;
+﻿using SolidPrincable.LSP;
+using SolidPrincable.OCP;
 using SolidPrincable.OCP_2;
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace SolidPrincable
 {
@@ -9,26 +12,69 @@ namespace SolidPrincable
         static void Main(string[] args)
         {
             /// console main thred
-            Manager1 emp = new Manager1
-            {
-                EmpId = 2,
-                empName = "Mohamed mohsen",
-                Salary = 3000,
-                OvertimeHours = 10
-            };
-            Regular1 emp2 = new Regular1
-            {
-                EmpId = 2,
-                empName = "Ahmed Goda",
-                Salary = 3000,
-                OvertimeHours = 10
-            };
-            var bonus = emp.CalclauteBounse();
-            var bounsReg = emp2.CalclauteBounse();
+            /// OCP Code
 
-            Console.WriteLine($"EmployeeDate :EmpID {emp.EmpId} EmployeeName : {emp.empName} \n BaseSalary {emp.Salary} \n Hours {emp.OvertimeHours} \n Bonus :{bonus}");
-            Console.WriteLine($"EmployeeDate :EmpID {emp2.EmpId} EmployeeName : {emp2.empName} \n BaseSalary {emp2.Salary} \n Hours {emp2.OvertimeHours} \n Bonus :{bounsReg}");
-            Console.ReadLine();
+            #region OCP-Code
+            //Manager1 emp = new Manager1
+            //{
+            //    EmpId = 2,
+            //    empName = "Mohamed mohsen",
+            //    Salary = 3000,
+            //    OvertimeHours = 10
+            //};
+            //Regular1 emp2 = new Regular1
+            //{
+            //    EmpId = 2,
+            //    empName = "Ahmed Goda",
+            //    Salary = 3000,
+            //    OvertimeHours = 10
+            //};
+            //var bonus = emp.CalclauteBounse();
+            //var bounsReg = emp2.CalclauteBounse();
+
+            //Console.WriteLine($"EmployeeDate :EmpID {emp.EmpId} EmployeeName : {emp.empName} \n BaseSalary {emp.Salary} \n Hours {emp.OvertimeHours} \n Bonus :{bonus}");
+            //Console.WriteLine($"EmployeeDate :EmpID {emp2.EmpId} EmployeeName : {emp2.empName} \n BaseSalary {emp2.Salary} \n Hours {emp2.OvertimeHours} \n Bonus :{bounsReg}");
+            //Console.ReadLine(); 
+            #endregion
+
+            PostDatabase db = new PostDatabase();
+            List<string> userPosts = new List<string>();
+            userPosts.Add("original Post");
+            userPosts.Add("#tag Post");
+            userPosts.Add("@mention post");
+
+            Post postobj;
+            foreach (var post in userPosts)
+            {
+                if (post.StartsWith("#"))
+                {
+                    postobj = new TagPost();
+                }
+                else if (post.StartsWith("@"))
+                {
+                    postobj = new MentionPost();
+                }
+                else
+                {
+                    postobj = new Post();
+                }
+
+                string result = postobj.CreatePost(db, post);
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
